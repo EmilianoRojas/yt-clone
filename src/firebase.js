26401b5704f38ef3d72376d38df3674556c982ba
EmilianoRojas <inversiones.socony@gmail.com>
2020-08-20 11:34:33 -0400

    initial setts

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBexZIoLLDQsJQgj29qu7lyGMPIyXOAjeQ",
+  authDomain: "yt-clone-7fb74.firebaseapp.com",
+  databaseURL: "https://yt-clone-7fb74.firebaseio.com",
+  projectId: "yt-clone-7fb74",
+  storageBucket: "yt-clone-7fb74.appspot.com",
+  messagingSenderId: "447028759022",
+  appId: "1:447028759022:web:183f5f2e3900eb5e6326ff",
+  measurementId: "G-4JHPM02KQG"
+};
